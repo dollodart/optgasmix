2ace66adfc473ea0106d18b442a9e9c75aaffcf7
david <davidollodart@gmail.com>
2021-02-19 17:58:25 -0800

    added n-dimensional uniform random sampling
    randomly samples the flat dirichlet distribution to obtain a uniform
    random sample of n-dimensonal composition space.

--- rand.py
+++ rand.py
@@ -1,8 +1,11 @@
+import itertools as it
 def gen_random_3(fineness=10):
     """
 
-    For three variables, a Cartesian product enumeration with one condition is equivalent to
-    geometric construction by an equilateral triangle to uniformly sample the composition space (create a uniform grid). 
+    For three variables, a Cartesian product enumeration with one
+    condition is equivalent to geometric construction by an equilateral
+    triangle to uniformly sample the composition space (create a uniform
+    grid).
 
     """
     sqrt3 = 3**(1/2)
@@ -26,28 +29,77 @@ def gen_random_3(fineness=10):
                 xs.append((d1, d2, round(1 - d1 - d2, 3)))
     return xs
 
+def gen_random_n(n,fineness=10):
+    combs = it.product(range(fineness + 1),repeat=n)
+    keep = []
+    for comb in combs:
+        if sum(comb) == fineness:
+            keep.append(tuple(x/fineness for x in comb))
+    return keep
+
+import numpy as np
+def gen_random_n_fast(n, K=None):
+    """
+
+    Generates random samples n-dimensional data from the flat Dirichlet distribution.
+
+    Input: 
+      n: the dimension of the composition space desired
+      K: the number of random samples desired
+    Output:
+      samples: K-by-n array containing K samples
+    """ 
+    if K is None:
+        K = 10**n
+    # overdraw, since you will have to discard many samples
+    x1 = np.random.random(10*n*K)
+    x2 = np.random.random(10*n*K)
+    bl = np.exp(-x1) > x2
+    samples = x1[bl][:n*K]
+    samples = samples.reshape((K,n)) 
+    norm = samples.sum(axis=1)
+    return samples / norm[:, np.newaxis]
+
 if __name__ == '__main__':
+    from time import time
+    t0 = time()
     xs = gen_random_3()
+    print(time() - t0)
     x1, x2, x3 = zip(*xs)
     x1 = sorted(x1)
     x2 = sorted(x2)
     x3 = sorted(x3)
 
     for x, y, z in zip(x1, x2, x3):
-        print(x, y, z)
+        pass
+        #print(x, y, z)
 
-#xs = []
-#for d1 in linspace:
-#    for d2 in linspace:
-#        if 1 - d1 - d2 >=0 :
-#            for d3 in linspace:
-#                if 1 - d1 - d2 - d3 >= 0:
-#                    xs.append((d1, d2, d3, round( 1 - d1 - d2 - d3, 3)))
-#
-#x1, x2, x3, x4 = zip(*xs)
-#x1 = sorted(x1)
-#x2 = sorted(x2)
-#x3 = sorted(x3)
-#x4 = sorted(x4)
-#for t in zip(x1,x2,x3,x4):
-#    print(' '.join(str(x) for x in t))
+    t0 = time()
+    xs = gen_random_n(4,10)
+    print(time() - t0)
+    x1, x2, x3, x4 = zip(*xs)
+    x1 = sorted(x1)
+    x2 = sorted(x2)
+    x3 = sorted(x3)
+    x4 = sorted(x4)
+    for t in zip(x1,x2,x3,x4):
+        pass
+        #print(' '.join(str(x) for x in t))
+
+
+    t0 = time()
+    xs = gen_random_n_fast(4,10**4)
+    print(xs[0])
+    print(time() - t0)
+    #import matplotlib.pyplot as plt
+    # visually inspect tetrahedron (expensive)
+    #from mpl_toolkits.mplot3d import Axes3D
+    #fig = plt.figure()
+    #ax = fig.add_subplot(111, projection='3d')
+    #ax.scatter(xs[:,0], xs[:,1], xs[:,2])
+    #plt.show()
+    # inspect histograms for uniform random values
+    #plt.hist(xs[:,0])
+    #plt.hist(xs[:,1])
+    #plt.hist(xs[:,2])
+    #plt.show()
